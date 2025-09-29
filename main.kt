80393cabe322b899e0e3064525fdc62e1cab6ebe
Hansen Quindao <hcquindao@up.edu.ph>
2025-09-30 01:03:17 +0800

    Added Scanner class and keywords list

--- main.kt
+++ main.kt
@@ -1,12 +1,62 @@
+// Class for the Scanner
+class Scanner(private val source: String) {
+    private val tokens = mutableListOf<Token>()
+    private var start = 0
+    private var current = 0
+    private var line = 1
+
+    // Keywords List
+    private val keywords = setOf(
+        "/function", "/kill", "/say", "/input", "/summon", "/expr",
+        "/execute", "/gamerule", "/effect", "/team",
+        "run", "else",
+        "and", "or", "not", 
+        "in", "is",
+        "as", "break", "pass"
+    )
+
+    // Multiword Keywords List
+    private val multiWordKeywords = listOf(
+        "/execute if",
+        "/execute for", 
+        "/execute while",
+        "not in",
+        "is not"
+    )
+
+    // Scan Token function
+    fun scanTokens(): List<Token> {
+        while (!isAtEnd()) {
+            start = current
+            scanToken()
+        }
+        tokens.add(Token(TokenType.EOF, "", null, line))
+        return tokens
+    }
+
+    private fun scanToken() {
+        val c = advance()
+        // placeholder: real scanning to come
+        println("Scanned char: $c")
+    }
+
+    private fun isAtEnd(): Boolean = current >= source.length
+    private fun advance(): Char = source[current++]
+
+
+}
+
+
+// Main function
 fun main() {
     println("Loading...")
-    println("Welcome to PyCraft (type 'quit' to exit)")
+    println("Welcome to PyCraft (type '/kill' to exit)")
 
     while (true) {
         print("> ")
         val input = readln()
 
-        if (input.trim() == "quit") break
+        if (input.trim() == "/kill") break
 
         val scanner = Scanner(input)
         val tokens = scanner.scanTokens()
